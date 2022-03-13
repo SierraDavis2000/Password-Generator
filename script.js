c1d674ed37dcf400f5e234d6850f450ecffc0ba2
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-03-12 23:17:29 -0500

    password length confirmation

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//variables for password elements
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+function generatePassword(){
+  var length = prompt("Please select the length of your password. Must be between 8 and 128 characters")
+  length = parseInt(length);
+  if (length < 8) {
+    alert ("Password must have at least 8 characters!");
+    return"";
+  }
+  if (length > 128){
+    alert("Password must be less than 128 characters!");
+    return"";
+  }
+
+  var charType = prompt ("Please select ")
+}
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-var length = number(prompt("How many characters would you like your password to be? Please select a number between 8 and 128."))
-var characterType = prompt("Enter a character type: lowercase, uppercase, numeric, special")
+generateBtn.addEventListener("click", function () {
+  writePassword();
+});
